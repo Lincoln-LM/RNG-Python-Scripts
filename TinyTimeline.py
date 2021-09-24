ceaf65cbee96545d59233b05e5b1e6a60da30e0a
Lincoln-LM <maestaslincoln1@gmail.com>
2021-09-24 02:13:08 -0600

    advanced tiny timeline

--- TinyTimeline.py
+++ TinyTimeline.py
@@ -5,20 +5,41 @@ class TinyTimeline(TinyMT):
         self.advances = advances
         self.tiny_advancers = tiny_advancers
         self.index = 0
+        self.times_cache = []
         super().__init__(state=initial_state)
         self.advance(index)
 
-    def next_time(self):
+    def generate_times(self):
         # tiny_advancer = [tiny_advance_type*,last_end_advance]
         # *(https://github.com/wwwwwwzx/3DSRNGTool/blob/68883c3be831f4ccb2dcb5aee22d7fcc734b4aae/3DSRNGTool/Gen6/TinyTimeline.cs#L60)
+        times = []
         self.advances = self.tiny_advancers[0][1]
         cooldown = self.getCooldown(self.tiny_advancers[0][0])
-        
+        times.append([self.advances+2,self.advances+cooldown,self.index,*self.state])
         self.tiny_advancers.append([self.tiny_advancers[0][0],self.advances+cooldown])
         self.tiny_advancers.pop(0)
         self.tiny_advancers = sorted(self.tiny_advancers, key=lambda x:x[1])
+        front = self.next() < 0x55555555
+        times.append(["-","-",self.index,*self.state])
+        if front:
+            front_cooldown = self.getCooldown(2)
+            self.advances = self.tiny_advancers[0][1]
+            self.tiny_advancers[0][1] = self.advances + front_cooldown
+            times.append([self.advances+2,self.advances+front_cooldown,self.index,*self.state])
         
-        return [self.advances+2,self.tiny_advancers[0][1],self.index,*self.state]
+        return times
+    
+    def next_time(self,include_unhittable=False):
+        if len(self.times_cache) == 0:
+            self.times_cache = self.generate_times()
+        time = ["-"]
+        while time[0] == "-":
+            if len(self.times_cache) == 0:
+                self.times_cache = self.generate_times()
+            time = self.times_cache[0]
+            self.times_cache.pop(0)
+        return time
+
 
     def nextState(self):
         self.index += 1
--- tiny_timeline_example.py
+++ tiny_timeline_example.py
@@ -1,12 +1,15 @@
 from TinyTimeline import TinyTimeline
 advances = 0
-index = 0
-state = [0xC5AC44ED,0x316727AD,0x77FE69BB,0x6BFBE324]
+index = 118 - 1
+state = [0x114D3525,0x66C2F888,0xC572CFAD,0x1EB60DB7]
 # tiny_advancer[0] = type 
 # tiny_advancer[1] = advance
-tiny_advancers = [[7,0],[7,2],[7,4],[7,6]]
+tiny_advancers = [[1,1601 - 2,False]]
 ttt = TinyTimeline(initial_state=state,advances=advances,index=index,tiny_advancers=tiny_advancers)
 print(f"Start\t~\tEnd \tIndex \tState")
-for _ in range(56):
+for _ in range(10):
     time = ttt.next_time()
-    print(f"{time[0]}\t~\t{time[1]} \t{time[2]} \t{time[6]:08X},{time[5]:08X},{time[4]:08X},{time[3]:08X}")
+    if time[1] <= time[0]:
+        print(f"-\t~\t- \t{time[2]} \t{time[6]:08X},{time[5]:08X},{time[4]:08X},{time[3]:08X}")
+    else:
+        print(f"{time[0]}\t~\t{time[1]} \t{time[2]} \t{time[6]:08X},{time[5]:08X},{time[4]:08X},{time[3]:08X}")
