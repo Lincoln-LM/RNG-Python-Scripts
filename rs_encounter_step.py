ae7cda432ab379ea2bd040e6c7d74ebd636a8c4b
Lincoln-LM <maestaslincoln1@gmail.com>
2021-10-04 17:38:19 -0600

    add rs_encounter_step

---
+++ rs_encounter_step.py
@@ -0,0 +1,45 @@
+from LCRNG import PokeRNG
+from tools import getIVs
+
+
+natures = ['Hardy','Lonely','Brave','Adamant','Naughty','Bold','Docile','Relaxed','Impish','Lax','Timid','Hasty','Serious','Jolly','Naive','Modest','Mild','Quiet','Bashful','Rash','Calm','Gentle','Sassy','Careful','Quirky']
+compatabilities = [20,50,70]
+methods = {1:(0,0),2:(1,0),4:(0,1)}
+
+# edit
+initial_seed = 0x5A0 # initial seed
+initial_advances = 0 # starting advances
+max_advances = 1000 # total advances to show
+compatability = 0 # 0: dont, 1: get along, 2: very well
+method = 1 # 1/2/4
+rate = 13 # ? get from the rom for your location
+show_non_encounter = False # whether or not to show lows with no encounters
+# ----
+
+rng = PokeRNG(initial_seed)
+rng.advance(initial_advances)
+
+for cnt in range(max_advances):
+    go = PokeRNG(rng.seed)
+    if go.nextUShort() * 100 // 0xFFFF < compatabilities[compatability]:
+        low = go.nextUShort() % 0xfffe + 1
+        if go.nextUShort() % 2880 < rate*16:
+            go.advance(2) # encounter slot
+            
+            search_nature = go.nextUShort() % 25
+            pid = -1
+            while pid % 25 != search_nature:
+                low = go.nextUShort()
+                high = go.nextUShort()
+                pid = (high << 16) | low
+
+            go.advance(methods[method][0])
+            iv1 = go.nextUShort()
+            go.advance(methods[method][1])
+            iv2 = go.nextUShort()
+            ivs = getIVs(iv1,iv2)
+        
+            print(f"{initial_advances+cnt} {low:04X} {pid:08X} {natures[search_nature]} {'/'.join(str(iv) for iv in ivs)}")
+        elif show_non_encounter:
+            print(f"{initial_advances+cnt} {low:04X} No Encounter")
+    rng.nextUInt()
--- tiny_timeline_example.py
+++ tiny_timeline_example.py
@@ -1,14 +1,16 @@
 from TinyTimeline import TinyTimeline
-advances = 0
-index = 133
+from TinyMT import TinyMT
+index = 4293
 initial_state = [0x114D3525,0x66C2F888,0xC572CFAD,0x1EB60DB7]
 # tiny_advancer[0] = type 
 # tiny_advancer[1] = advance
-tiny_advancers = [[1,2989,False]]
+tiny_advancers = [[6,28271,False]]
 ttt = TinyTimeline(initial_state=initial_state,index=index,tiny_advancers=tiny_advancers)
 print(f"Start\t~\tEnd \tIndex \tState")
-for _ in range(10):
+for _ in range(30):
     time = ttt.next_time()
+    tmt = TinyMT(state=time[3:7])
+    print(((tmt.next() * 100) >> 32) & 0xFFFFFFFF)
     if time[1] <= time[0]:
         print(f"-\t~\t- \t{time[2]} \t{time[6]:08X},{time[5]:08X},{time[4]:08X},{time[3]:08X}")
     else:
